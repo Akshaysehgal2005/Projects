1c3b44369dcf1f1e14efb89853acfb4dc8cd6ff0
Akshay Sehgal <akshaysehgal2005@gmail.com>
2021-04-09 09:41:15 +0530

    updated notebooks

---
+++ Random/vim_python.py
@@ -0,0 +1,21 @@
+#VIM TEST CODE
+
+#This is a test file to play around with VIM Editor
+
+import numpy as np
+
+arr = np.random.randint(0,120,10)
+user_input = int(input('type a number'))
+
+print(arr[:user_input])
+
+
+
+
+
+
+
+
+
+
+
